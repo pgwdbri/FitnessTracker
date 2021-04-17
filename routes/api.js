4e8027e50ce87e08171d482058decc2ad5efe617
Brianna Shade <bri.krystalle@gmail.com>
2021-04-17 14:53:38 -0400

    routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,30 @@
 var app = require("express").Router()
 var db = require("../model/fitness")
 
-app.get("/", function(req,res){
+app.get("/api/workouts", function(req,res){
     db.find().then(function(records){
-        console.log(records)
+        console.log("GET",records)
         res.json(records)
-    }))
+    })
 })
+app.put("/api/workouts/:id", function(req,res){
+    db.findByIdAndUpdate(req.params.id,
+        {$push: {exercises:req.body}},{new:true}).then(function(records){
+        console.log("PUT",records)
+        res.json(records)
+    })
+})
+app.post("/api/workouts", function(req,res){
+    db.create(req.body).then(function(records){
+        console.log("POST",records)
+        res.json(records)
+    })
+})
+
+app.get("/api/workouts/range", function(req,res){
+    db.find().limit(10).then(function(records){
+        console.log("GET",records)
+        res.json(records)
+    })
+})
+module.exports = app
